e5a548b62b33ee6ad1a756c92bd218db7d8155ac
KuldeepSinh <kuldeepsinh.singh3@gmail.com>
2018-12-20 21:43:05 -0500

    added Pocket algorithm

---
+++ classifiers/Pocket.py
@@ -0,0 +1,43 @@
+from BaseClass import Classifier
+import numpy as np
+from copy import deepcopy as copy
+
+class PocketAlgorithm(Classifier):
+    """
+        class for Pocket Algorithm 
+    """
+    def __init__(self,maxiter=500,alpha=0.1):
+        Classifier.__init__(self)
+        self.w = np.zeros((X.shape[1]))
+        self.w_pocket = copy(self.w)
+        self.maxiter = maxiter
+        self.alpha = alpha
+
+
+    def compare(self,X,T,w,wp):
+        y = np.sign(X @ w)
+        yp = np.sign(X @ wp)
+        return 1 if np.sum(y==T) >= np.sum(yp == T) else -1
+    
+    def train(self,X,T):
+        Xs = self.normalize(X)
+        for i in range(self.maxiter):
+            converged = True
+            for k in np.random.permutation(Xs.shape[0]):
+                y = self.w @ Xs[k]
+                np.sign(y)
+                np.sign(T[k])
+                if np.sign(y) != np.sign(T[k]):
+                    self.w += self.alpha * T[k] * Xs[k]
+                    converged = False
+                    if self.compare(Xs,T,self.w,self.w_pocket) > 0:
+                        self.w_pocket[:] = self.w[:]
+            if converged:
+                print("converged at ",i)
+                break
+        print("End of training: ",i)
+        
+    def use(self,X):
+        Xs = self.normalize(X)
+        #plt.plot(Xs@self.w_pocket)
+        return Xs@self.w_pocket
---
+++ classifiers/main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+
+    mu1 = [-1, -1]
+    cov1 = np.eye(2)
+
+    mu2 = [2,3]
+    cov2 = np.eye(2) * 3
+
+    C1 = np.random.multivariate_normal(mu1, cov1, 50)
+    C2 = np.random.multivariate_normal(mu2, cov2, 50)
+
+    plt.plot(C1[:, 0], C1[:, 1], 'or')
+    plt.plot(C2[:, 0], C2[:, 1], 'xb')
+
+    plt.xlim([-3, 6])
+    plt.ylim([-3, 7])
+
+    X = np.vstack((C1, C2))
+    N = X.shape[0]
+    T = np.ones(N)
+    T[:50] *= -1 
+
+    pocket = PocketAlgorithm()
+    pocket.train(X,T)
+    xt = np.array([-2, 5])
+    yt = -pocket.w_pocket[0] * xt / pocket.w_pocket[1]
+    plt.plot(xt,yt)
